b8a15a8740e200b8fb3322b080b2670948ae2ea6
Akshu-1312 <12888ds2023@princescience.in>
2025-04-06 16:21:48 +0530

    Update app.py
    Signed-off-by: Akshu-1312 <12888ds2023@princescience.in>

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ with open("IRAA Truth Detector.html", 'r', encoding='utf-8') as f:
     html_content = f.read()
 
 # Display the HTML
-st.components.v1.html(html_content, height=600, scrolling=True)
+st.components.v1.html(html_content,height=600,scrolling=True)
