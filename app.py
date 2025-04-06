9becbadf7afb8047110e93ca5d0c88d5a39c8f18
Akshu-1312 <12888ds2023@princescience.in>
2025-04-06 16:19:13 +0530

    Create app.py
    Signed-off-by: Akshu-1312 <12888ds2023@princescience.in>

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# Load your HTML file
+with open("IRAA Truth Detector.html", 'r', encoding='utf-8') as f:
+    html_content = f.read()
+
+# Display the HTML
+st.components.v1.html(html_content, height=600, scrolling=True)
